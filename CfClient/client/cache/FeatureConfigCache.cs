3b658e85cde06085dfcb195958cb8e512deb8940
AleczeHarness <aleksandar.samardzija@harness.io>
2021-06-25 09:38:32 +0200

    Init

---
+++ CfClient/client/cache/FeatureConfigCache.cs
@@ -0,0 +1,46 @@
+﻿using io.harness.cfsdk.HarnessOpenAPIService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace io.harness.cfsdk.client.cache
+{
+    public class FeatureConfigCache : IMyCache<string, FeatureConfig>
+    {
+        public Dictionary<string, FeatureConfig> CacheMap { get; set; }
+
+        public FeatureConfigCache()
+        {
+            CacheMap = new Dictionary<string, FeatureConfig>();
+        }
+
+        public void Put(string key, FeatureConfig value)
+        {
+            if (CacheMap.ContainsKey(key))
+            {
+                CacheMap.Remove(key);
+            }
+            CacheMap.Add(key, value);
+        }
+
+        public FeatureConfig getIfPresent(string key)
+        {
+            if (!CacheMap.ContainsKey(key))
+            {
+                return null;
+            }
+            return CacheMap[key];
+        }
+
+        public Dictionary<string,FeatureConfig> GetAllElements()
+        {
+            return CacheMap;
+        }
+
+        public void Delete(String key)
+        {
+            CacheMap.Remove(key);
+        }
+    }
+}
---
+++ CfClient/client/cache/ICache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace io.harness.cfsdk.client.cache
+{
+    public interface ICache<K, V>
+    {
+
+        public void PutAll(ICollection<KeyValuePair<K, V>> keyValuePairs)
+        {
+            foreach (var item in keyValuePairs)
+            {
+                Put(item);
+            }
+        }
+
+        public void Put(K key, V value);
+        public void Put(KeyValuePair<K, V> keyValuePair)
+        {
+            Put(keyValuePair.Key, keyValuePair.Value);
+        }
+        public V getIfPresent(K  key);
+    }
+}
