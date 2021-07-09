832607f9a8029169370da677b94eb3653a83ddfc
milos85vasic-harness <milos.vasic@harness.io>
2021-07-09 08:51:06 +0200

    FFM-940 - Consistent use of the cluster parameter.

--- CfClient/client/api/SSEListener.cs
+++ CfClient/client/api/SSEListener.cs
@@ -16,13 +16,13 @@ namespace io.harness.cfsdk.client.api
         private string cluster;
         private CfClient cfClient;
 
-        public SSEListener(DefaultApi defaultApi, FeatureConfigCache featureCache, SegmentCache segmentCache, string environmentID, string clusterIdentifier, CfClient cfClient)
+        public SSEListener(DefaultApi defaultApi, FeatureConfigCache featureCache, SegmentCache segmentCache, string environmentID, string cluster, CfClient cfClient)
         {
             this.defaultApi = defaultApi;
             this.featureCache = featureCache;
             this.segmentCache = segmentCache;
             this.environmentID = environmentID;
-            this.cluster = clusterIdentifier;
+            this.cluster = cluster;
             this.cfClient = cfClient;
         }
 
